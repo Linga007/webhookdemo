95bfd9071d0ce09612b1a4bdd6af6353c3d927d4
Linga <Lingaraou007@gmail.com>
2022-09-11 17:25:40 +0530

    secound commit

--- src/main/java/com/example/webhookdemo/WebhookdemoApplication.java
+++ src/main/java/com/example/webhookdemo/WebhookdemoApplication.java
@@ -2,11 +2,16 @@ package com.example.webhookdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class WebhookdemoApplication {
 
+
 	public static void main(String[] args) {
+
+		System.out.println("welcome to webhook");
 		SpringApplication.run(WebhookdemoApplication.class, args);
 	}
 
